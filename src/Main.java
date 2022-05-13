f5d6d3f3009bcc3caf518fb885b6328c9fb66616
Mo Awad <mhmdawaddd@gmail.com>
2022-05-13 18:34:44 +0200

    isValid question

--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,34 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(">>>>>>>>>?? "+strWithout3a3b(1,1));
+        System.out.println(">>>>>>>>>?? "+isValid("([)]"));
 
     }
-
+    public static boolean isValid(String s) {
+        if(s.length() % 2 != 0){
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
+        for(char c : s.toCharArray()){
+            switch (c){
+                case '(':
+                    stack.add(')');
+                    break;
+                case '[':
+                    stack.add(']');
+                    break;
+                case '{':
+                    stack.add('}');
+                    break;
+                default:{
+                    if(stack.isEmpty() || stack.pop() != c){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
 
 }
 
