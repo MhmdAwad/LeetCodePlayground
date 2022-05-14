ebd9159248283eadd22711f378529211ba1c7209
Mo Awad <mhmdawaddd@gmail.com>
2022-05-14 11:12:39 +0200

    maxArea question

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,28 @@
 
-import java.util.*;
-import java.util.stream.Collectors;
-
-import static problem_solving.easy.longestCommonPrefix.longestCommonPrefix;
-import static problem_solving.meduim.StrWithout3a3b.strWithout3a3b;
-
-
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(">>>>>>>>>?? "+searchInsert(new int[]{1,3,5,6}, 4));
-
+        int[] nums = new int[]{1,8,6,2,5,4,8,3,7};
+        int[] nums2 = new int[]{3,9,3,4,7,2,12,6};
+        int k = maxArea(nums2);
+        System.out.println(">>>>>>>>>?? "+ k);
     }
-    public static int searchInsert(int[] nums, int target) {
-        int first = 0, last = nums.length-1;
-        // 1  3  5  6
-        //
+    public static int maxArea(int[] height) {
+        int first=  0, last = height.length-1;
+        int area = 0;
         while (first < last){
-            int mid = first + (last - first) / 2;
-            if(nums[mid] == target) return mid;
-            else if(nums[mid] > target){
-                last = mid-1;
-            }else{
-                first = mid+1;
+            area = Math.max(Math.min(height[first], height[last]) * (last - first), area);
+            if(height[first] < height[last]){
+                first++;
+            }else {
+//                area = Math.max(height[last] * (last - first), area);
+                last--;
             }
-        }
-        return first;
-    }
 
+
+        }
+        return area;
+    }
 }
 
