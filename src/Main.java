981b5f4b8f356359fa7c826a6dc295f4e3b611b6
Mo Awad <mhmdawaddd@gmail.com>
2022-05-16 11:17:42 +0200

    productExceptSelf question

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,33 @@
 import problem_solving.meduim.AddTwoNumbers.ListNode;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        int[] nums = new int[]{7, 1, 5, 3, 6, 4};
-        int[] nums1 = new int[]{1, 2, 3, 4, 5};
-        ListNode node = new ListNode(1);
-        ListNode node1 = new ListNode(2);
-        ListNode node2 = new ListNode(3);
-        ListNode node3 = new ListNode(4);
-        ListNode node4 = new ListNode(5);
-        ListNode node5 = new ListNode(6);
-        node.next = node1;
-        node1.next = node2;
-        node2.next = node3;
-        node3.next = node4;
-        node4.next = node5;
-        ListNode k = swapPairs(node);
-        while (k != null) {
-            System.out.println(">>>>>>>>>?? " + k.val);
-            k = k.next;
-        }
+//        int[] nums = new int[]{2,3,6,7};
+        int[] nums = new int[]{1,2,3,4};
+
+//        System.out.println(">>>>>>? " + combinationSum(nums, 7));
+        productExceptSelf(nums);
     }
 
-    public static ListNode swapPairs(ListNode head) {
-        if (head == null || head.next == null) return head;
-        ListNode second = head.next;
-        ListNode res = head.next;
-        head.next = second.next;
-        second.next = head;
-        second.next.next = swapPairs(head.next);
-        return res;
+    public static int[] productExceptSelf(int[] nums) {
+        int length = nums.length;
+        int[] ans = new int[length];
+        int last = 1;
+        ans[0] = 1;
+        for (int i =1; i < length; i++){
+            ans[i] = ans[i-1] * nums[i-1];
+        }
+        for(int i = length-1; i >= 0; i--){
+            ans[i] *= last;
+            last *= nums[i];
+        }
+        return ans;
     }
 }
 
