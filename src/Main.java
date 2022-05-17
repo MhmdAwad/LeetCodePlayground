5ad7e300926306e14850df6bef474d7d918587d6
Mo Awad <mhmdawaddd@gmail.com>
2022-05-17 17:48:49 +0200

    threeSum medium question

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,49 @@
 import problem_solving.meduim.AddTwoNumbers.ListNode;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
 //        int[] nums = new int[]{2,3,6,7};
-        int[] nums = new int[]{1,2,3,4};
+//        int[] nums = new int[]{4, 5, 6, 7, 0, 1, 2};
+        int[] nums = new int[]{-1,0,1,2,-1,-4,-2,-3,3,0,4};
 
-//        System.out.println(">>>>>>? " + combinationSum(nums, 7));
-        productExceptSelf(nums);
+
+        System.out.println(">>>>>>? " + threeSum(nums));
+
     }
 
-    public static int[] productExceptSelf(int[] nums) {
-        int length = nums.length;
-        int[] ans = new int[length];
-        int last = 1;
-        ans[0] = 1;
-        for (int i =1; i < length; i++){
-            ans[i] = ans[i-1] * nums[i-1];
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+
+        if(nums.length < 3) return list;
+        Arrays.sort(nums);
+
+        for (int i=0; i<nums.length; i++){
+            if(i > 0 && (nums[i-1] == nums[i]))
+                continue;
+
+            int left = i+1 , right = nums.length-1;
+            while (left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum == 0){
+                    List<Integer> tmp = new ArrayList<>();
+                    tmp.add(nums[i]);
+                    tmp.add(nums[left]);
+                    tmp.add(nums[right]);
+                    list.add(tmp);
+                    left++;
+                    while ((nums[left] == nums[left-1]) && (left < right)) left++;
+                } else if (sum > 0) {
+                    right--;
+                }else {
+                    left++;
+                }
+            }
         }
-        for(int i = length-1; i >= 0; i--){
-            ans[i] *= last;
-            last *= nums[i];
-        }
-        return ans;
+        return list;
     }
 }
 
